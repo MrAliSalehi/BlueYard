7c1ec862e2432d2890e53555a0a6e04bb3079b4d
Qwxp <mmdjoker@yahoo.com>
2022-02-26 03:07:54 +0330

    add lang js

---
+++ bundle/Lang.js
@@ -0,0 +1,18 @@
+var languages = {
+  english: {
+    test: "english test",
+  },
+  Turkish: {
+    test: "turkish test",
+  },
+};
+
+var dataReload = document.querySelectorAll("[data-reload]");
+
+dataReload.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    setTimeout(function () {
+      window.location.reload();
+    });
+  });
+});
