beea3f489a680eaa66218b0e97773d9c81207063
Qwxp <mmdjoker@yahoo.com>
2022-02-27 12:44:21 +0330

    new translations

--- bundle/Lang.js
+++ bundle/Lang.js
@@ -1,16 +1,48 @@
 //language dictionaries
 var languages = {
-  en: { begintext: "begin" },
+  en: {
+    begintext: "begin",
+    investing: "Investing in founders creating the",
+    fabric: "FABRIC OF OUR FUTURE",
+    blueyard:
+      "BlueYard backs founders building the interconnected elements that can become the fabric of our future.",
+    explore: "EXPLORE THE FUTURE",
+    scenius: "SCENIUS",
+    web3: "WEB3",
+    programmable: "programmable",
+    biology: "Biology",
+    breakthrough: "GRAFİK TASARIM & 3B ÇÖZÜMLER",
+    computation: "3 BOYUT",
+    liberated: "liberated",
+    knowledge: "knowledge & data",
+    about: "about",
+    blueyard: "blueyard",
+  },
 
-  tr: { begintext: "BAŞLAT" },
+  tr: {
+    begintext: "BAŞLAT",
+    investing: "YENİLİKÇİ FİKİRLER, TEKNOLOJİLER VE TASARIMLAR",
+    fabric: "GELECEĞE BİZİM İLE",
+    blueyard:
+      "Digital Project Verse; özel yazılım geliştirme,mobil uygulama geliştirme, kurumsal web tasarım iş ve oyun  sektörlerinde 3B çözümler sunuyoruz",
+    explore: "GELECEĞE GÖZATIN",
+    scenius: "WEB, UYGULAMA, SİBER GÜVENLİK",
+    web3: "YAZILIM",
+    programmable: "BİZE ULAŞIN",
+    biology: "İLETİŞİM",
+    breakthrough: "breakthrough",
+    computation: "computation & engineering",
+    liberated: "BİZ KİMİZ",
+    knowledge: "EKİP",
+    about: "HAKKIMIZDA",
+    blueyard: "DPV",
+  },
 };
 
 //set language on load
 window.onload = function () {
   if (window.location.hash) {
     const multiLangElements = document.querySelectorAll("[data-lang]");
-    console.log("Elements to Change Count: " + multiLangElements.length);
-
     changeContent(multiLangElements);
   }
 };
