557cf25d611660f8e7501071b83e1da1625889b5
Qwxp <mmdjoker@yahoo.com>
2022-03-02 08:10:23 +0330

    fix mail-1

--- feedback.php
+++ feedback.php
@@ -3,8 +3,6 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $name = $_POST['name'];
 $headers = "From:$email" . "\r\n";
-// use wordwrap() if lines are longer than 70 characters
-$message = wordwrap($message, 70);
 
 // send email
 mail("mmdjoker@yahoo.com", "FeedBack-$name", $message, $headers);
